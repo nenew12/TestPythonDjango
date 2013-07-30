be2bbf0e49f6cb2a80007c4a4570e8d2e1893a6a
Nenew <nenew57@gmail.com>
2013-07-30 23:35:04 +0700

    new project

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-from pythonTestSite.view import hello
+from pythonTestSite.view import hello, Fuck_you
  
 urlpatterns = patterns('',
-    url(r'^hello/$', hello),               
+    url(r'^hello/$', hello),
+    url(r'^FuckYou/$', Fuck_you),               
 )
--- view.py
+++ view.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
  
 def hello(request):
     return HttpResponse("Hello world website with python")
+
+def Fuck_you(request):
+	return HttpResponse("Test Fuck you")
